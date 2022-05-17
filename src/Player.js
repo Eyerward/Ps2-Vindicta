3393ba32be5a9abed9c5ac04c82f60717d9d31a1
kancabricot <matthieubruffa@gmail.com>
2022-05-17 08:21:08 +0200

    et là

--- src/Player.js
+++ src/Player.js
@@ -7,7 +7,7 @@ class Player {
         this.life = 20;
         this.power = 0;
 
-
+//et là
         this.player = this.scene.physics.add.sprite(600, 5700, 'player');
         //Taille de la hitbox du Player
         this.player.body.setSize(this.player.width-70, this.player.height).setOffset(35, 0);
