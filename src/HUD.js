32dec4f32b851ad9bd477afa8783f588a181e96a
Eyerward <92451560+Eyerward@users.noreply.github.com>
2022-06-02 23:05:29 +0200

    va vraiment falloir faire gaffe

--- src/HUD.js
+++ src/HUD.js
@@ -1,5 +1,10 @@
 class HUD extends Phaser.scene {
     constructor(){
-        
+        super('HUD')
+    }
+
+    preload(){
+        this.load.image('player_base', 'assets/images/ui_player.png');
+
     }
 }
